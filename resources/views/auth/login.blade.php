44fae83067947280910a3a8fadbc18bbb6f5004c
nixon-dev <nickson.prieto005@gmail.com>
2025-04-08 16:07:28 +0800

    Added label for each form input

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,35 +1,35 @@
 @extends('base.auth')
 @section('title', 'Login - Management Information System')
 @section('form')
-<div class="col-md-6 d-md-block">
-    <div class="ibox-content border-radius-35 dark-skin">
-        <h2 class="font-bold text-center">Login</h2>
-        <form class="m-t" role="form" action="" method="POST">
-            @csrf
-            <div class="form-group">
-                <input type="email" name="inputEmail"
-                    class="form-control dark-skin-2 text-white border-secondary" placeholder="Email"
-                    required="">
-            </div>
-            <div class="form-group">
-                <input type="password" name="inputPassword"
-                    class="form-control dark-skin-2 text-white border-secondary" placeholder="Password"
-                    required="">
-            </div>
-            <label> <input type="checkbox" class="i-checks" name="inputRememberMe"> Remember me </label>
-            <button type="submit" class="btn btn-primary block full-width m-b">Login</button>
+    <div class="col-md-6 d-md-block">
+        <div class="ibox-content border-radius-35 dark-skin">
+            <h2 class="font-bold text-center">Login</h2>
+            <form class="m-t" role="form" action="" method="POST">
+                @csrf
+                <div class="form-group">
+                    <label>Email</label>
+                    <input type="email" name="inputEmail" class="form-control dark-skin-2 text-white border-secondary"
+                        required>
+                </div>
+                <div class="form-group">
+                    <label>Password</label>
+                    <input type="password" name="inputPassword" class="form-control dark-skin-2 text-white border-secondary"
+                        required>
+                </div>
+                <label> <input type="checkbox" class="i-checks" name="inputRememberMe"> Remember me </label>
+                <button type="submit" class="btn btn-primary block full-width m-b">Login</button>
 
-            <a href="#">
-                <small>Forgot password?</small>
-            </a>
+                <a href="#">
+                    <small>Forgot password?</small>
+                </a>
 
-            <p class="text-muted text-center">
-                <small>Do not have an account?</small>
-            </p>
-            <a class="btn btn-sm btn-success block full-width m-b" href="{{ url('/register') }}">Create
-                an
-                account</a>
-        </form>
+                <p class="text-muted text-center">
+                    <small>Do not have an account?</small>
+                </p>
+                <a class="btn btn-sm btn-success block full-width m-b" href="{{ url('/register') }}">Create
+                    an
+                    account</a>
+            </form>
+        </div>
     </div>
-</div>
 @endsection
--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -7,31 +7,35 @@
             <form class="m-t" role="form" action="{{ route('register') }}" method="POST">
                 @csrf
                 <div class="form-group">
+                    <label>Name</label>
                     <input type="text" name="name" class="form-control dark-skin-2 text-white border-secondary"
-                        placeholder="Name" value="{{ old('name') }}" required>
+                        value="{{ old('name') }}" required>
                     @error('name')
                         <span class="text-danger">{{ $message }}</span>
                     @enderror
 
                 </div>
                 <div class="form-group">
+                    <label>Email</label>
                     <input type="email" name="email" class="form-control dark-skin-2 text-white border-secondary"
-                        placeholder="Email" value="{{ old('email') }}" pattern="[a-z0-9._%+\-]+@[a-z0-9.\-]+\.[a-z]{2,}$"
+                        value="{{ old('email') }}" pattern="[a-z0-9._%+\-]+@[a-z0-9.\-]+\.[a-z]{2,}$"
                         title="Must be valid email" required>
                     @error('email')
                         <span class="text-danger">{{ $message }}</span>
                     @enderror
                 </div>
                 <div class="form-group">
+                    <label>Password</label>
                     <input type="password" name="password" class="form-control dark-skin-2 text-white border-secondary"
-                        placeholder="Password" required minlength="6">
+                        required minlength="6">
                     @error('password')
                         <span class="text-danger">{{ $message }}</span>
                     @enderror
                 </div>
                 <div class="form-group">
+                    <label>Confirm Password</label>
                     <input type="password" name="password_confirmation"
-                        class="form-control dark-skin-2 text-white border-secondary" placeholder="Confirm Password" required
+                        class="form-control dark-skin-2 text-white border-secondary" required
                         minlength="6">
                 </div>
                 <button type="submit" class="btn btn-primary block full-width m-b">Register</button>
